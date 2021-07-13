d4b70ea342d4f00b756c1fe723680d507c0d52ac
mohammadali1383 <mohammadalifatalahzade@gmail.com>
2021-07-13 07:18:01 -0700

    initial commit

---
+++ music.js
@@ -0,0 +1,20 @@
+const songs = [{
+    music1 : {
+        id : '1'
+        artis : 'armin'
+        year : '1998'
+    }
+    },{
+    music2 : {
+        id : '2'
+        artis : 'mohsen'
+        year : '2010'
+    }
+    },{
+    music3 : {
+        id : '3'
+        artis : 'hosein'
+        year : '2019'
+    }
+    }]
+    console . log (song [0] . music1 . armin) ;
